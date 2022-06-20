b30043a65c4d2b40422510c07d81d30dba9955e5
Klyde <klydefb@gmail.com>
2022-06-20 11:54:14 +0800

    List and unfinished receipt

--- main.py
+++ main.py
@@ -19,89 +19,65 @@ class Food:
 
 
 
-def main():
-    f1 = Food("Fishball", 5)
-    f2 = Food("Kwek-kwek", 10)
-    f3 = Food("Tempura", 5)
-    f4 = Food("Lumpia", 10)
-    f5 = Food("Isaw", 5)
-    f6 = Food("Hotdog", 20)
-    f7 = Food("Chicken skin", 5)
-    f8 = Food("Squid skin", 5)
-    f9 = Food("Siomai", 15)
+# Store name and price in a list
+food_list = \
+    [
+        Food("Fishball", 5),
+        Food("Kwek-kwek", 10),
+        Food("Tempura", 5),
+        Food("Lumpia", 10),
+        Food("Isaw", 5),
+        Food("Hotdog", 20),
+        Food("Chicken skin", 5),
+        Food("Squid skin", 5),
+        Food("Siomai", 15)
+    ]
 
+
+
+def food_menu():
+    # Display menu using loop
     print("AVAILABLE TODAY:")
-    print("\t[1] ", f1.get_name(), "     ..........  ₱", f1.get_price())
-    print("\t[2] ", f2.get_name(), "    ..........  ₱", f2.get_price())
-    print("\t[3] ", f3.get_name(), "      ..........  ₱", f3.get_price())
-    print("\t[4] ", f4.get_name(), "       ..........  ₱", f4.get_price())
-    print("\t[5] ", f5.get_name(), "         ..........  ₱", f5.get_price())
-    print("\t[6] ", f6.get_name(), "       ..........  ₱", f6.get_price())
-    print("\t[7] ", f7.get_name(), " ..........  ₱", f7.get_price())
-    print("\t[8] ", f8.get_name(), "   ..........  ₱", f8.get_price())
-    print("\t[9] ", f9.get_name(), "       ..........  ₱", f9.get_price())
-    print("\n----------")
-    order_food = input("Enter your orders:  ")
+    for i in range(len(food_list)):
+        print("\t[", str(i + 1), "] ", food_list[i].get_name(), " .......... ₱",  str(food_list[i].get_price()))
+    print("\n\n----------")
+
+
+
+
+
+def order(food_list):
+    # Get orders of the user
+    order_food = input("Enter your orders: ")
     order_food = order_food.split()
-    print("\n\nORDER SUCCESSFULLY ADDED_")
 
-
+    order_food = food_list()
     # Total ordered foods of each type
-    f1_total = 0
-    f2_total = 0
-    f3_total = 0
-    f4_total = 0
-    f5_total = 0
-    f6_total = 0
-    f7_total = 0
-    f8_total = 0
-    f9_total = 0
+    food_total = [0] * len(food_list)
 
-    # For this warning, read here: https://stackoverflow.com/questions/51800809/warning-when-python-access-2d-list-with-none-value
+    # Loop to get the total of each food type
     for i in range(len(order_food)):
-        order_food[i] = int(order_food[i])         # Convert each item to int type
+        # Convert each item to int type
+        order_food[i] = int(order_food[i])
 
-        if order_food[i] == 1:
-            f1_total = f1_total + 1
-        elif order_food[i] == 2:
-            f2_total = f2_total + 1
-        elif order_food[i] == 3:
-            f3_total = f3_total + 1
-        elif order_food[i] == 4:
-            f4_total = f4_total + 1
-        elif order_food[i] == 5:
-            f5_total = f5_total + 1
-        elif order_food[i] == 6:
-            f6_total = f6_total + 1
-        elif order_food[i] == 7:
-            f7_total = f7_total + 1
-        elif order_food[i] == 8:
-            f8_total = f8_total + 1
-        elif order_food[i] == 9:
-            f9_total = f9_total + 1
+        if order_food[i] in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            food_total[order_food[i] - 1] += 1
+
 
     # Display ordered food
-    print("\n\n+ - - - - - - - - - - - - - - - - +")
-    print("|                                 |")
-    print("|        *** RECEIPT ***          |")
-    print("|                                 |")
-    print("|                                 |")
-    print("|   ", f1_total, f1.get_name(), "    .....  ₱", f1.get_price() * f1_total, "   |")
-    print("|   ", f2_total, f2.get_name(), "   .....  ₱", f2.get_price() * f2_total, "   |")
-    print("|   ", f3_total, f3.get_name(), "     .....  ₱", f3.get_price() * f3_total, "   |")
-    print("|   ", f4_total, f4.get_name(), "      .....  ₱", f4.get_price() * f4_total, "   |")
-    print("|   ", f5_total, f5.get_name(), "        .....  ₱", f5.get_price() * f5_total, "   |")
-    print("|   ", f6_total, f6.get_name(), "      .....  ₱", f6.get_price() * f6_total, "   |")
-    print("|   ", f7_total, f7.get_name(), ".....  ₱", f7.get_price() * f7_total, "   |")
-    print("|   ", f8_total, f8.get_name(), "  .....  ₱", f8.get_price() * f8_total, "   |")
-    print("|   ", f9_total, f9.get_name(), "      .....  ₱", f9.get_price() * f9_total, "   |")
-    print("|                                  |", )
-    print("+----------------------------------+")
+    print("\n\n*** RECEIPT ***")
+    for i in range(len(food_list)):
+        print("\t ", str(food_total[i]), food_list[i].get_name(), " ( ₱ ", str(food_list[i].get_price() * food_total[i]), ")")
+    print("----------")
+    print("TOTAL PRICE: ", )
+    print("----------")
 
+    return food_list
 
 
 
 
 
 if __name__ == "__main__":
-    main()
+    food_menu()
+    order(food_list)
